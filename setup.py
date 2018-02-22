b20f5efb417ee62ac378bc2ed927e2fbec0c381d
Martin Casey <caseym3@tcd.ie>
2018-02-22 11:42:16 +0000

    adjustment to names

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='flask_test',
+    name='learning_flask',
     version='0.1.0',
     description='learning flask basics',
     author='Martin Casey',
     author_email='martin.casey@ucdconnect.ie',
-    url='https://github.com/MartinC20xx/flask_test',
+    url='https://github.com/MartinC20xx/learning_flask',
     packages=['app'],
     entry_points={
-          "console_scripts":['comp30670_flask_test=flask_test.run:main']})
+          "console_scripts":['comp30670_learning_flask=flask_test.run:main']})
 #check this entry point once issue with run.py is resolved. 
 
