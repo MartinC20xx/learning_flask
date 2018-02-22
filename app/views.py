d7890a905369297823665c4fe1fb77c94744d823
Martin Casey <caseym3@tcd.ie>
2018-02-22 11:37:34 +0000

    Added import statement to view for testing purposes

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from app import app
+from systeminfo import main
+#added import for testing reasons
 
 @app.route('/')
 def index():
     returnDict = {}
     returnDict['user'] = 'Martin'
     returnDict['title'] = 'Home'
+   
     return render_template("index.html", **returnDict)
