9b2abc0607308264226b538e04fbab90c725d7eb
Martin Casey <caseym3@tcd.ie>
2018-02-22 11:45:26 +0000

    correction to a name

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     url='https://github.com/MartinC20xx/learning_flask',
     packages=['app'],
     entry_points={
-          "console_scripts":['comp30670_learning_flask=flask_test.run:main']})
+          "console_scripts":['comp30670_learning_flask=app.run:main']})
 #check this entry point once issue with run.py is resolved. 
 
